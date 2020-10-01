290f447d1c84c264a5d1ebf10b7b27e14c890751
tchutka <tristan.chutka@outlook.com>
2020-09-30 20:03:21 -0700

    parallelized assn2 for assn3 (added assn2 to this branch). Current issue: squished video. not tested on rpi

---
+++ assns/assn3/sobel.cpp
@@ -0,0 +1,150 @@
+
+#include <iostream>
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace cv;
+using namespace std;
+
+enum VEC3B_COLORS
+{
+    BLUE,
+    GREEN,
+    RED
+};
+
+enum EVENT_FLAGS
+{
+    WAITING,
+    READY,
+    DONE
+};
+struct Frame
+{
+    uint8_t flags;
+    Mat* frame;
+};
+
+#define SOBEL_ROWS 3
+#define SOBEL_COLS 3
+struct sobel_weight
+{
+    const int (*w_x)[SOBEL_ROWS];
+    const int (*w_y)[SOBEL_ROWS];
+};
+
+uchar ITU_R(uchar R, uchar G, uchar B)
+{
+    return .0722 * B + .7152 * G + .2126 * R;
+}
+
+void gray_scal(uchar (*gray_alg)(uchar, uchar, uchar), Mat &img, Mat &gray)
+{
+    int rows = img.rows;
+    int cols = img.cols;
+
+    gray.create(img.size(), CV_8UC1);
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            Vec3b &channels = img.at<Vec3b>(row, col);
+            gray.at<uchar>(row, col) = gray_alg(channels[RED], channels[GREEN], channels[BLUE]);
+        }
+    }
+}
+
+uchar window_product(Mat &gray, const struct sobel_weight *W, int startX, int startY)
+{
+    int product = 0;
+    for (int row = startY; row < startY + 3; row++)
+    {
+        for (int col = startX; col < startX + 3; col++)
+        {
+            product += W->w_x[row - startY][col - startX] * gray.at<uchar>(col, row);
+            product += W->w_y[row - startY][col - startX] * gray.at<uchar>(col, row);
+        }
+    }
+    if (product > 255)
+    {
+        return 255;
+    }
+    else if (product < 0)
+    {
+        return 0;
+    }
+    return product;
+}
+
+void sobel_filter(const struct sobel_weight *W, Mat &gray, Mat &sobel)
+{
+    int rows = gray.rows;
+    int cols = gray.cols;
+    sobel.create(gray.size(), gray.type());
+
+    for (int row = 1; row < rows - 1; row++)
+        for (int col = 1; col < cols - 1; col++)
+            sobel.at<uchar>(row, col) = window_product(gray, W, row - 1, col - 1);
+}
+
+int main(int argc, char *argv[])
+{
+    VideoCapture inputVideo;
+    double displayTime = 33.36;
+    Mat img, gray, sobel;
+    const int W_x[SOBEL_ROWS][SOBEL_COLS] = {
+        {1, 0, -1},
+        {2, 0, -2},
+        {1, 0, -1}};
+    const int W_y[SOBEL_ROWS][SOBEL_COLS] = {
+        {1, 2, 1},
+        {0, 0, 0},
+        {-1, -2, -1}};
+    const struct sobel_weight WEIGHTS = {W_x, W_y};
+
+    //parse args
+    if (argc < 2)
+    {
+        cerr << "sobel <video_file> " << endl;
+        exit(EXIT_FAILURE);
+    }
+    if (argc == 3)
+    {
+        if (argv[2][0] == '-')
+        {
+            if (argv[2][1] == 'i')
+                displayTime = 0;
+        }
+    }
+
+    if (!inputVideo.open(argv[1]))
+    {
+        cerr << "Not able to open " << argv[1] << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        inputVideo >> img;
+        if (img.empty()) {
+            break;
+        } else {
+            //make new process thread
+            //with id for circular buffer
+        }
+
+        //join
+        //if data avail, add to framebuffer based on ID
+
+        // Step 1 - get gray mat
+        gray_scal(ITU_R, img, gray);
+        // Step 2 - get sober filter
+        sobel_filter(&WEIGHTS, gray, sobel);
+        imshow("sobel filter", sobel);
+        waitKey(displayTime);
+    }
+
+    return 0;
+}
