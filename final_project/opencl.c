9110d52e9c1839b9d7b1022e954a05718bb91389
byvictorrr <vdelaplainess@gmail.com>
2020-11-20 06:52:39 -0800

    back to rasp

--- final_project/opencl.c
+++ final_project/opencl.c
@@ -1,12 +1,19 @@
-#include <opencl-c.h>
+#include "opencl-c.h"
+#include "openv.h"
 
-__kernel void windowProduct(__global __image2d_t a,
+/*
+kernel void windowProduct(__global __image2d_t a,
+                            __global __image2d_t b,
                            __global uchar *res)
 {
     a[i]
 }
+*/
 
 int main(){
 
+cl_int ret_code = CL_SUCCESS;
+cl_mem shore_mask_buff = clCreateBuffer(ocl_context, CL_MEM_COPY_HOST_PTR,                                         
+                                        shore_mask.cols * shore_mask.rows * sizeof(uint8_t), (void*)shore_mask.data, &ret_code);
 
 }
