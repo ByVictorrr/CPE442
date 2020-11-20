f15d3c8c92e5150077caa8a43a13a3e773691ae5
victor delaplaine <vdelaplainess@gmail.com>
2020-11-20 06:25:07 -0800

    final projec

---
+++ final_project/perf_test.c
@@ -0,0 +1,16 @@
+#include <linux/perf_event.h>
+#include <linux/bpf_perf_event.h>
+
+/*
+static int libperf_print(enum libperf_print_level level, const char *fmt, va_list ap){
+    return vprintf(stderr, fmt, ap);
+}
+*/
+#include "libperf.h"
+
+int main(){
+
+    struct libperf_data *pd = libperf_initialize(-1,-
+    
+    return 0;
+}
---
+++ final_project/threadhandler.h
@@ -0,0 +1,39 @@
+#ifndef __THREAD_HANDLER__
+#define __THREAD_HANDLER__
+
+#include <sys/types.h>
+
+typedef void *(*t_func)(void *);
+struct t_arg
+{
+    u_int8_t init;
+    size_t id;
+};
+
+class ThreadHandler
+{
+private:
+    pthread_t *t_handles;
+    struct t_arg *t_args;
+    size_t t_handles_idx;
+    size_t t_count;
+
+    size_t get_next_thread(void);
+
+public:
+    ThreadHandler(size_t max_threads);
+    ~ThreadHandler();
+
+    int create(void *data_in, t_func func);
+    int create(size_t id, void *data_in, t_func func);
+    int join(size_t t_id, void **data_out);
+    int joinAll(void *data_out, size_t size);
+};
+
+/*
+inline void* mmapfile(void *, size_t, size_t, int fd);
+inline void* umapfile(void*, size_t);
+double dtime(void);
+*/
+
+#endif
