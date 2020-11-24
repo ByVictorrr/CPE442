aea2b7f69717eb447d4104e179c7cadd3bfc9b47
Tristan Chutka <tristan.chutka@outlook.com>
2020-11-23 19:16:06 -0800

    added sobel kernel. streamlined code

--- final_project/kernels.cl
+++ final_project/kernels.cl
@@ -1,4 +1,4 @@
-__kernel void gray_scale(__global uchar *input, __global  uchar *output)
+__kernel void Gray(__global unsigned char* input, __global unsigned char* output)
 {
     int gid = get_global_id(0);
     // BGR
@@ -8,30 +8,28 @@ __kernel void gray_scale(__global uchar *input, __global  uchar *output)
     output[gid] =  0.0722 * input[3*gid+B] + 0.7152 * input[3*gid+G] + 0.2126 * input[3*gid+R];
 }
 
-__kernel void sobel_product(__global uchar *gray, __global uchar *sobel, uchar size, uint width, uint height)
+__kernel void Sobel(__global unsigned char* gray, __global unsigned char* sobel, unsigned int size, unsigned int width, unsigned int height)
 {
-    int id = get_global_id(0);
-    id *= size;
-    //int max_index = (width * height) / size;
-    int x = id % width;
-    int y = id / width;
+    int x = get_global_id(0);
+    int y = get_global_id(1);
 
-    if ((x == 0) || (y == 0) || (x == (width - 1)) || (y == (heigh - 1)))
+    if ((x > 0) && (x < (width-1)) && (y > 0 && (y < (height-1))))
     {
-        sobel[id] = 0;
-        return;
+        int x_product = (1 * gray[width * (y-1) + (x-1)]) + (0 * gray[width * (y-1) + (x)]) + (-1 * gray[width * (y-1) + (x+1)]) +
+                        (2 * gray[width * (y  ) + (x-1)]) + (0 * gray[width * (y  ) + (x)]) + (-2 * gray[width * (y  ) + (x+1)]) +
+                        (1 * gray[width * (y+1) + (x-1)]) + (0 * gray[width * (y+1) + (x)]) + (-1 * gray[width * (y+1) + (x+1)]);
+
+        int y_product = ( 1 * gray[width * (y-1) + (x-1)]) + ( 2 * gray[width * (y-1) + (x)]) + ( 1 * gray[width * (y-1) + (x+1)]) +
+                        ( 0 * gray[width * (y  ) + (x-1)]) + ( 0 * gray[width * (y  ) + (x)]) + ( 0 * gray[width * (y  ) + (x+1)]) +
+                        (-1 * gray[width * (y+1) + (x-1)]) + (-2 * gray[width * (y+1) + (x)]) + (-1 * gray[width * (y+1) + (x+1)]);
+        
+        int product = x_product + y_product;
+        if (product > 255) {
+            product = 255;
+        }
+        if (product < 0) {
+            product = 0;
+        }
+        sobel[width * y + x] = product;
     }
-    if ((x >= width) || (y >= height))
-        return;
-
-    // index = (width * y) + x;
-    int x_product = (1 * gray[(width * (y-1) + (x-1)]) + (0 * gray[(width * (y-1) + (x)]) + (-1 * gray[(width * (y-1) + (x+1)]) +
-                    (2 * gray[(width * (y  ) + (x-1)]) + (0 * gray[(width * (y  ) + (x)]) + (-2 * gray[(width * (y  ) + (x+1)]) +
-                    (1 * gray[(width * (y+1) + (x-1)]) + (0 * gray[(width * (y+1) + (x)]) + (-1 * gray[(width * (y+1) + (x+1)]);
-
-    int y_product = ( 1 * gray[(width * (y-1) + (x-1)]) + ( 2 * gray[(width * (y-1) + (x)]) + ( 1 * gray[(width * (y-1) + (x+1)]) +
-                    ( 0 * gray[(width * (y  ) + (x-1)]) + ( 0 * gray[(width * (y  ) + (x)]) + ( 0 * gray[(width * (y  ) + (x+1)]) +
-                    (-1 * gray[(width * (y+1) + (x-1)]) + (-2 * gray[(width * (y+1) + (x)]) + (-1 * gray[(width * (y+1) + (x+1)]);
-                    
-    sobel[id] = x_product + y_product;
 }
--- final_project/sobel.cpp
+++ final_project/sobel.cpp
@@ -1,137 +1,51 @@
+#define CL_TARGET_OPENCL_VERSION 120
+
 #include <iostream>
 #include <ostream>
 #include <istream>
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/ocl.hpp>
-
-#include <CL/cl.hpp>
 #include <CL/cl.h>
 
-#define KERNEL_SOURCE "kernels.cl"
-
-class ArgParser{
-    public:
-        static const char *parse(int argc, char**argv){
-            if (argc != 2)
-            {
-                std::cerr << "sobel <video_file> " << std::endl;
-                exit(EXIT_FAILURE);
-            }
-            return argv[1];
-        }
-};
-/*
-std::string readKernel(const char *fileName){
+std::string readKernel(const char *fileName)
+{
     std::ifstream file(fileName);
-     std::string content( (std::istreambuf_iterator<char>(file) ),
-                       (std::istreambuf_iterator<char>()    ) );
+    std::string content((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
     return content;
 }
 
-cv::Mat grayScale(cv::Mat &regular, cl_context context, cl_command_queue queue)
+int main(int argc, char **argv)
 {
-    cv::Mat gray;
-    gray.create(regular.size(), CV_8UC1);
-    /*
-    std::cout << "size: " << regular.size() << std::endl;
-    std::cout << "cols: " << regular.cols << std::endl;
-    std::cout << "rows: " << regular.rows << std::endl;
-    std::cout << "rows*cols: " << regular.rows*regular.cols << std::endl;
-    * /
-    std::string sourceGray = readKernel("gray_scale.cl");
-    const char *_sourceGray = sourceGray.c_str();
-
-    size_t globalThreads= regular.rows * regular.cols;
-    cl_mem regularIMG, grayIMG;
-    cl_int err;
-    cl_program program = clCreateProgramWithSource(context, 1, &_sourceGray, NULL, &err);
-    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    cl_kernel kernel = clCreateKernel(program, "gray_scale", &err);
-
-
-    // allocate the first buffer (src)
-    regularIMG = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(uchar) * regular.cols * regular.rows * 3, NULL, &err);
-    grayIMG = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(uchar)* regular.cols * regular.rows, NULL, &err) ;
-    // Write our data set into the input array in device memory
-    err = clEnqueueWriteBuffer(queue, regularIMG, CL_TRUE, 0, regular.cols*regular.rows*3, regular.data, 0, NULL, NULL);
-     
-    // setting kernel arguments
-    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&regularIMG);
-    err = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&grayIMG);
-    // Enqueue a command to execute a kernel on device
-    err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalThreads, NULL, 0, NULL, NULL);
-    err = clFinish(queue);
-    std::cout << "finished" << std::endl;
-
-    // read back the result
-    err = clEnqueueReadBuffer(queue, grayIMG, CL_TRUE, 0, (size_t)regular.rows*regular.cols, (void*)gray.ptr<uchar>(0), 0, NULL, NULL);
-    return gray;
-}
-
-cv::Mat sobel(cv::Mat &regular, cl_context context, cl_command_queue queue){
-    cv::Mat gray = grayScale(regular, context, queue);
-    std::string sourceWP = readKernel("window_product.cl");
-    const char *_sourceWP = sourceWP.c_str();
-    size_t globalThreads= regular.rows * regular.cols;
-    cl_program program;
-    cl_kernel kernel; 
-    cl_int err;
-
-    cl_mem grayMEM;
-    const cl_image_format format = {
-        .image_channel_order=CL_R, 
-        .image_channel_data_type=CL_UNSIGNED_INT8
-    };
-    const cl_image_desc = {
-        .image_type=CL_MEM_OBJECT_IMAGE2D,
-        .image_width=gray.cols,
-        .image_height=gray.rows,
-        .image_depth=0, // only for 3d images
-        .image_array_size=0, // only if using type CL_MEM_OBJECT_IMAGE<2|1>D_ARRAY
-        .image_row_pitch=,
-        .image_slice_pitch=,
-        .num_mip_levels=,
-        .num_samples=,
-        .buffer=
-    };
-
-    program = clCreateProgramWithSource(context, 1, &_sourceWP, NULL, &err);
-    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    kernel = clCreateKernel(program, "window_product", &err);
-
-    // create images
-    grayMEM = clCreateImage(context, CL_MEM_READ_ONLY, format, );
-
-
-}
-*/
-
-
-int main(int argc, char **argv){
+    if (argc != 2)
+    {
+        std::cerr << "sobel <video_file> " << std::endl;
+        exit(EXIT_FAILURE);
+    }
     //cv::setNumThreads(0);
     cv::VideoCapture inputVideo;
     cv::Mat img;
-    
+
     //open and read first frame of input video to get matrix sizes
-    if(!inputVideo.open(argv[1])){
-    	std::cout << "error opening video" << std::endl;
-	    exit(EXIT_FAILURE);
+    if (!inputVideo.open(argv[1]))
+    {
+        std::cout << "error opening video" << std::endl;
+        exit(EXIT_FAILURE);
     }
-    inputVideo >> img;  
+    inputVideo >> img;
 
     //opencl vars
     cl_int err;
-    cl_platform_id platform ;
+    cl_platform_id platform;
     cl_context context;
     cl_program program;
     cl_device_id device_id;
     cl_command_queue queue;
     char str_buffer[1024];
-    size_t DATA_SIZE = sizeof(uchar) * img.cols * img.rows;
+    size_t DATA_SIZE = sizeof(unsigned char) * img.cols * img.rows;
 
     err = clGetPlatformIDs(1, &platform, NULL);
-    if (err!= CL_SUCCESS)
+    if (err != CL_SUCCESS)
     {
         printf("Unable to get platform_id\n");
         exit(EXIT_FAILURE);
@@ -145,68 +59,96 @@ int main(int argc, char **argv){
     }
     //if device found, print info
     err = clGetDeviceInfo(device_id, CL_DEVICE_NAME, sizeof(str_buffer), &str_buffer, NULL);
-    printf("%s", str_buffer);
+    printf("%s\n", str_buffer);
     // create a context with the GPU device
     context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
     // create command queue using the context and device
-    queue = clCreateCommandQueue (context, device_id, 0, &err);
+    queue = clCreateCommandQueue(context, device_id, 0, &err);
 
-    program = clCreateProgramWithSource(context, 1, (const char **) KERNEL_SOURCE, NULL, &err);
-   //compile the program
-    if (clBuildProgram(program, 0, NULL, NULL, NULL, NULL) != CL_SUCCESS)
+    std::string kernel_source = readKernel("kernels.cl");
+    const char *_kernel_source = kernel_source.c_str();
+    program = clCreateProgramWithSource(context, 1, &_kernel_source, NULL, &err);
+    //compile the program
+    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    if (err != CL_SUCCESS)
     {
-        printf("Error building program\n");
+        //clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 1024, str_buffer, NULL);
+        //printf("Error building program: %d : %s\n", err, str_buffer);
+        printf("Error building program: %d\n", err);
         exit(EXIT_FAILURE);
     }
     // select kernels from the program to execute
-    cl_kernel gray_kernel = clCreateKernel(program, "gray_scale", &err);
-    cl_kernel sobel_kernel = clCreateKernel(program, "sobel_product", &err);
+    cl_kernel gray_kernel = clCreateKernel(program, "Gray", &err);
+    if (err != CL_SUCCESS)
+    {
+        printf("gray kernel failed: %d\n", err);
+        exit(EXIT_FAILURE);
+    }
+    cl_kernel sobel_kernel = clCreateKernel(program, "Sobel", &err);
+    if (err != CL_SUCCESS)
+    {
+        printf("sobel kernel failed: %d\n", err);
+        exit(EXIT_FAILURE);
+    }
 
     // create buffers for the input and ouput
-    cl_mem input = clCreateBuffer(context, CL_MEM_READ_WRITE, DATA_SIZE * 3, NULL, NULL); //pixel of depth 3
+    cl_mem input = clCreateBuffer(context, CL_MEM_READ_WRITE, DATA_SIZE * 3, NULL, NULL);   //pixel of depth 3
     cl_mem gray_output = clCreateBuffer(context, CL_MEM_READ_WRITE, DATA_SIZE, NULL, NULL); //this is read/write to allow it to be reused without copying data
     cl_mem sobel_output = clCreateBuffer(context, CL_MEM_READ_WRITE, DATA_SIZE, NULL, NULL);
 
-    while(1) {
+    while (1)
+    {
         // load data into the input buffer
-        err = clEnqueueWriteBuffer(queue, input, CL_TRUE, 0, DATA_SIZE * 3, img.data, 0, NULL, NULL);
+        err = clEnqueueWriteBuffer(queue, input, CL_TRUE, 0, DATA_SIZE * 3, img.data, 0, NULL, NULL); //img.ptr<uchar>(0)
 
         // set the argument list for the Grayscale kernel command
         clSetKernelArg(gray_kernel, 0, sizeof(cl_mem), &input);
         clSetKernelArg(gray_kernel, 1, sizeof(cl_mem), &gray_output);
 
         size_t global_work_size = DATA_SIZE;
+        //DEBUG std::cout << "img rows: " << img.rows << " img cols: " << img.cols << " work size: " << global_work_size << std::endl;
         // enqueue the kernel command for execution
-        clEnqueueNDRangeKernel(queue, gray_kernel, 1, NULL, &global_work_size, NULL, 0, NULL, NULL);
+        err = clEnqueueNDRangeKernel(queue, gray_kernel, 1, NULL, &global_work_size, NULL, 0, NULL, NULL);
+        if (err != CL_SUCCESS)
+        {
+            printf("gray kernel failed: %d\n", err);
+            exit(EXIT_FAILURE);
+        }
+        err = clFinish(queue);
 
-        clFinish(queue);
+        unsigned int step = 3;
+        unsigned int width = img.cols;
+        unsigned int height = img.rows;
+        // set the argument list for the sobel kernel command
+        clSetKernelArg(sobel_kernel, 0, sizeof(cl_mem), &gray_output);
+        clSetKernelArg(sobel_kernel, 1, sizeof(cl_mem), &sobel_output);
+        clSetKernelArg(sobel_kernel, 2, sizeof(unsigned int), &step);
+        clSetKernelArg(sobel_kernel, 3, sizeof(unsigned int), &width);
+        clSetKernelArg(sobel_kernel, 4, sizeof(unsigned int), &height);
 
-        uchar sobel_kernel_step = 3;
-       // set the argument list for the sobel kernel command
-        clSetKernelArg(sobel_kernel, 0, sizeof(cl_mem), &input);
-        clSetKernelArg(sobel_kernel, 1, sizeof(cl_mem), &gray_output);
-        clSetKernelArg(sobel_kernel, 2, sizeof(uchar), &sobel_kernel_step);
-        clSetKernelArg(sobel_kernel, 3, sizeof(img.width), img.width);
-        clSetKernelArg(sobel_kernel, 4, sizeof(img.height), img.height);
-        // add more args here
         //perform sobel kernel
-        global_work_size = DATA_SIZE / (sobel_kernel_step * sobel_kernel_step); //no need for 1 thread per pixel, only 1 per step^2 pixels
-        clEnqueueNDRangeKernel(queue, sobel_kernel, 1, NULL, &global_work_size, NULL, 0, NULL, NULL);
+        const unsigned int sobel_work_size[2] = {width, height}; //x,y
+        err = clEnqueueNDRangeKernel(queue, sobel_kernel, 2, NULL, sobel_work_size, NULL, 0, NULL, NULL);
+        if (err != CL_SUCCESS)
+        {
+            printf("sobel kernel failed: %d\n", err);
+            exit(EXIT_FAILURE);
+        }
 
-        cv::Mat sobel;
-        sobel.create(img.size(), CV_8UC1);
-        
+        cv::Mat sobel(img.size(), CV_8UC1);
+
         // copy the results from out of the output buffer
-        clEnqueueReadBuffer(queue, sobel_output, CL_TRUE, 0, DATA_SIZE, sobel.data, 0, NULL, NULL);
-        
-        clFinish(queue);
+        err = clEnqueueReadBuffer(queue, sobel_output, CL_TRUE, 0, DATA_SIZE, sobel.data, 0, NULL, NULL); //sobel_output
+        err = clFinish(queue);
 
         cv::imshow("sobel", sobel);
         cv::waitKey(33.36); // 29.97 fps
+        //DEBUG cv::waitKey(0);
 
         //q next frame
-        image >> img;
-        if(img.empty()){
+        inputVideo >> img;
+        if (img.empty())
+        {
             std::cout << "last frame" << std::endl;
             break;
         }
@@ -219,12 +161,10 @@ int main(int argc, char **argv){
     clReleaseProgram(program);
     clReleaseKernel(gray_kernel);
     clReleaseKernel(sobel_kernel);
-    clReleaseCommandQueue(command_queue);
-    clReleaseContext(context)
+    clReleaseCommandQueue(queue);
+    clReleaseContext(context);
     inputVideo.release();
     cv::destroyAllWindows();
 
-
-   return 0;
-
+    return 0;
 }
